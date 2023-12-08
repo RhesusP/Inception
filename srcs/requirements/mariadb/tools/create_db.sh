a29c8f310d7f696d4a285db96800f5535e4b2a0c
cbernot <camil.bernot.cb@gmail.com>
2023-12-08 09:12:45 +0100

    🛢️ Mariadb container ok:x

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -1,10 +1,84 @@
 #!/bin/sh
 
-service mariadb start;
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-# mysql -e "FLUSH PRIVILEGES;"
-# mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-# exec mysqld_safe
+print_success() {
+	printf "\e[32m%s\e[0m\n" "$1"
+}
+
+print_failure() {
+	printf "\e[31m%s\e[0m\n" "$1"
+}
+
+mysql --version
+
+# Start the mysql daemon in the background.
+if service mariadb start; then
+	print_success "mariadb service started successfully ✔️"
+else
+	print_failure "mariadb service failed to start ❌"
+	exit 1
+fi
+
+# Create the 'inception_db' database if it doesn't exist.
+if mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"; then
+	print_success "database \`${MYSQL_DATABASE}\` created ✔️"
+else
+	print_failure "database \`${MYSQL_DATABASE}\` failed to create ❌"
+	exit 1
+fi
+
+# mysql -e "USE inception_db;"
+
+# Create the 'user' user if it doesn't exist.
+if mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USERNAME}\`@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';"; then
+	print_success "user \`${MYSQL_USERNAME}\` created ✔️"
+else
+	print_failure "user \`${MYSQL_USERNAME}\` failed to create ❌"
+	exit 1
+fi
+
+# Grant privileges to the 'user' user for the 'inception_db' database.
+if mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USERNAME}\`@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';"; then
+	print_success "user \`${MYSQL_USERNAME}\` granted ✔️"
+else
+	print_failure "user \`${MYSQL_USERNAME}\` failed to grant ❌"
+	exit 1
+fi
+
+# Make changes take effect
+if mysql -e "FLUSH PRIVILEGES;"; then
+	print_success "privileges flushed ✔️"
+else
+	print_failure "privileges failed to flush ❌"
+	exit 1
+fi
+
+echo "============================"
+mysql -e "SELECT Host, User, Password FROM mysql.user;"
+echo "============================"
+
+# Update root password
+if mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"; then
+	print_success "root password changed ✔️"
+else
+	print_failure "root password failed to change ❌"
+	exit 1
+fi
+
+echo "============================"
+mysql -e "SELECT Host, User, Password FROM mysql.user;"
+echo "============================"
+
+# Stop the mysql daemon to restart it with the new configuration.
+if mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown; then
+	print_success "mariadb service stopped successfully ✔️"
+else
+	print_failure "mariadb service failed to stop ❌"
+	exit 1
+fi
+
+if exec mysqld_safe; then
+	print_success "mariadb service restarted successfully ✔️"
+else
+	print_failure "mariadb service failed to restart ❌"
+	exit 1
+fi
