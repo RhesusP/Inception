d82bfd6c5f1ec73540c28346150b290f378fb3d9
cbernot <camil.bernot.cb@gmail.com>
2023-12-13 22:09:22 +0100

    ℹ️  improving information messages

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -1,74 +1,73 @@
 #!/bin/sh
 
 print_success() {
-	printf "\e[32m%s\e[0m\n" "$1"
+	printf "\e[32m ✔\e[0m %s\t\e[32msuccess\e[0m\n" "$1"
+
 }
 
 print_failure() {
-	printf "\e[31m%s\e[0m\n" "$1"
+	printf "\e[31m ✗\e[0m %s\t\e[31mfailed\e[0m\n" "$1"
 }
 
 # Start the mysql daemon in the background.
 if service mariadb start; then
-	print_success "mariadb service started successfully ✔️"
+	print_success "start mariadb service"
 else
-	print_failure "mariadb service failed to start ❌"
+	print_failure "start mariadb service"
 	exit 1
 fi
 
 # Create the 'inception_db' database if it doesn't exist.
 if mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"; then
-	print_success "database \`${MYSQL_DATABASE}\` created ✔️"
+	print_success "create database \`${MYSQL_DATABASE}\`"
 else
-	print_failure "database \`${MYSQL_DATABASE}\` failed to create ❌"
+	print_failure "create database \`${MYSQL_DATABASE}\`"
 	exit 1
 fi
 
-# mysql -e "USE inception_db;"
-
 # Create the 'user' user if it doesn't exist.
 if mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USERNAME}\`@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';"; then
-	print_success "user \`${MYSQL_USERNAME}\` created ✔️"
+	print_success "create user \`${MYSQL_USERNAME}\`"
 else
-	print_failure "user \`${MYSQL_USERNAME}\` failed to create ❌"
+	print_failure "create user \`${MYSQL_USERNAME}\`"
 	exit 1
 fi
 
 # Grant privileges to the 'user' user for the 'inception_db' database.
 if mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USERNAME}\`@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';"; then
-	print_success "user \`${MYSQL_USERNAME}\` granted ✔️"
+	print_success "user \`${MYSQL_USERNAME}\` granted"
 else
-	print_failure "user \`${MYSQL_USERNAME}\` failed to grant ❌"
+	print_failure "user \`${MYSQL_USERNAME}\` granted"
 	exit 1
 fi
 
 # Make changes take effect
 if mysql -e "FLUSH PRIVILEGES;"; then
-	print_success "privileges flushed ✔️"
+	print_success "privileges flushed"
 else
-	print_failure "privileges failed to flush ❌"
+	print_failure "privileges flushed"
 	exit 1
 fi
 
 # Update root password
 if mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"; then
-	print_success "root password changed ✔️"
+	print_success "update root password"
 else
-	print_failure "root password failed to change ❌"
+	print_failure "update root password"
 	exit 1
 fi
 
 # Stop the mysql daemon to restart it with the new configuration.
 if mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown; then
-	print_success "mariadb service stopped successfully ✔️"
+	print_success "stop mariadb service"
 else
-	print_failure "mariadb service failed to stop ❌"
+	print_failure "stop mariadb service"
 	exit 1
 fi
 
 if exec mysqld_safe; then
-	print_success "mariadb service restarted successfully ✔️"
+	print_success "restart mariadb service"
 else
-	print_failure "mariadb service failed to restart ❌"
+	print_failure "restart mariadb service"
 	exit 1
 fi
--- srcs/requirements/wordpress/conf/wp_config.sh
+++ srcs/requirements/wordpress/conf/wp_config.sh
@@ -1,76 +1,85 @@
 #!/bin/sh
 
 print_success() {
-	printf "\e[32m%s\e[0m\n" "$1"
+	printf "\e[32m ✔\e[0m %s\t\e[32msuccess\e[0m\n" "$1"
 }
 
 print_failure() {
-	printf "\e[31m%s\e[0m\n" "$1"
+	printf "\e[31m ✗\e[0m %s\t\e[31mfailed\e[0m\n" "$1"
+}
+
+print_info() {
+	printf "\e[33m%s\e[0m\n" "$1"
 }
 
 # Waiting for mariadb to start
-print_success "Waiting for mariadb to start..."
+print_info "Waiting 10s before starting mariadb service..."
 sleep 10;
 
 # Check if wp-config.php exists
 if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
-	print_success "wp-config.php not found, creating one..."
+	print_info "wp-config.php not found, creating one..."
 	wp config create --allow-root \
 					--dbname=$MYSQL_DATABASE \
 					--dbuser=$MYSQL_USERNAME \
 					--dbpass=$MYSQL_USER_PASSWORD \
 					--dbhost=mariadb:3306 \
-					--path='/var/www/wordpress'
+					--path='/var/www/wordpress' \
+					--quiet
+
 	if [ $? -eq 0 ]; then
-		print_success "wp-config.php created successfully ✔️"
+		print_success "create wp-config.php"
 	else
-		print_failure "wp-config.php creation failed ❌"
-		# exit 1
+		print_failure "create wp-config.php"
 	fi
 	sleep 5;
-	print_success "installing wordpress... to ${DOMAIN_NAME}"
+	# print_info "installing wordpress to ${DOMAIN_NAME}..."
 	wp core install --allow-root \
 					--url=$DOMAIN_NAME \
 					--title=$WORDPRESS_WP_NAME \
 					--admin_user=$WORDPRESS_ADMIN_USERNAME \
 					--admin_password=$WORDPRESS_ADMIN_PASSWORD \
 					--admin_email=$WORDPRESS_ADMIN_EMAIL \
-					--path='/var/www/wordpress'
+					--path='/var/www/wordpress' \
+					--quiet
 	if [ $? -eq 0 ]; then
-		print_success "wordpress installed successfully ✔️"
+		print_success "install wordpress"
 	else
-		print_failure "wordpress installation failed ❌"
+		print_failure "install wordpress"
 		exit 1
 	fi
-	print_success "creating user \`${WORDPRESS_USERNAME}\`... "
+	# print_info "creating user \`${WORDPRESS_USERNAME}\`... "
 	wp user create --allow-root \
 					$WORDPRESS_USERNAME \
 					$WORDPRESS_USER_EMAIL \
 					--user_pass=$WORDPRESS_PASSWORD \
 					--role=author \
-					--path='/var/www/wordpress'
+					--path='/var/www/wordpress' \
+					--quiet
 	if [ $? -eq 0 ]; then
-		print_success "user created successfully ✔️"
+		print_success "create user"
 	else
-		print_failure "user creation failed ❌"
+		print_failure "create user"
 		exit 1
 	fi
 
 	wp theme install astra \
 					--allow-root \
 					--activate \
-					--path='/var/www/wordpress'
+					--path='/var/www/wordpress' \
+					--quiet
 	if [ $? -eq 0 ]; then
-		print_success "theme installed successfully ✔️"
+		print_success "install theme"
 	else
-		print_failure "theme installation failed ❌"
+		print_failure "install theme"
 		exit 1
 	fi
 
 else
-	print_success "wp-config.php found, skipping configuration..."
+	print_info "wp-config.php found, skipping configuration..."
 fi
 
-wp --allow-root theme list --path='/var/www/wordpress'
+
+printf "\n\n\n\e[1m\e[32m🚀 Website is ready 🚀\e[0m\n"
 
 /usr/sbin/php-fpm7.3 -F
